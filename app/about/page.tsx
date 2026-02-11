cd81e361f5db1130467af3a156a4c49bb74ac0b1
Charlie <charlielautry@gmail.com>
2026-02-11 14:52:06 -0600

    Updated site

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -55,7 +55,7 @@ export default function About() {
                 <h3 className="text-2xl font-bold mb-2">Jase Scott</h3>
                 <p className="text-gray-400 mb-2">Secretary</p>
                 <p className="text-gray-400 mb-2">Computer Science</p>
-                <p className="text-gray-400">Junior</p>
+                <p className="text-gray-400">Year in School</p>
               </div>
             </div>
             <div className="bg-gray-800 rounded-lg overflow-hidden">
@@ -81,13 +81,13 @@ export default function About() {
                 <h3 className="text-2xl font-bold mb-2">Joshua Price</h3>
                 <p className="text-gray-400 mb-2">Outreach</p>
                 <p className="text-gray-400 mb-2">Computer Science</p>
-                <p className="text-gray-400">Senior</p>
+                <p className="text-gray-400">Year in School</p>
               </div>
             </div>
             <div className="bg-gray-800 rounded-lg overflow-hidden">
               <div className="h-[32rem] overflow-hidden">
                 <img 
-                  src="/images/charlesautry.JPEG" 
+                  src="/images/charlesautry.jpeg" 
                   alt="Charles Autry"
                   className="w-full h-full object-cover object-[center_20%]"
                 />
