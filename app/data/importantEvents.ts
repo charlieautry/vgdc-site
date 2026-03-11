b3f9980b764167d9b8f4ec123acd9a3178346642
Charlie <charlielautry@gmail.com>
2026-03-11 15:43:10 -0500

    Updated sites

--- app/data/importantEvents.ts
+++ app/data/importantEvents.ts
@@ -11,11 +11,11 @@ export interface ImportantEvent {
 
 export const importantEvents: ImportantEvent[] = [
   {
-    title: "Events Coming Soon!",
-    image: "/images/VGDC_Spring_2026_Flyer.PNG",
-    description: "Nothing to see here right now, but stay tuned for more information about our upcoming events!",
+    title: "The April Jam",
+    image: "/images/vgdc-chilljam1.jpg",
+    description: "From March 11th to April 1st, participate in our April Jam! Create a game based on the theme announced at our March 11th meeting. All skill levels are welcome, and you can work alone or in teams.",
     date: "Spring 2026",
-    link: "",
-    linkText: ""
+    link: "https://itch.io/jam/osu-spring-2026-chill-jam",
+    linkText: "Join the Jam!"
   }
 ];
